905f415b8d6326ffe2e306b0a83da471200d6466
leukone <scisly.a@gmail.com>
2016-10-16 01:43:09 +0200

    a

--- config/settings.py
+++ config/settings.py
@@ -133,12 +133,6 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
