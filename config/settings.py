e84700c5548e84bd2f6f5d860014ceed545968aa
leukone <scisly.a@gmail.com>
2016-10-16 22:39:10 +0200

    a

--- config/settings.py
+++ config/settings.py
@@ -142,12 +142,11 @@ AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
       'Cache-Control': 'max-age=94608000',
 }
 
-
 # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
 # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
 # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
 # We also use it in the next setting.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.s3-eu-west-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
