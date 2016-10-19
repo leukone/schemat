6d25c599aa1d3c1c8456e01636d43523cd53b039
leukone <scisly.a@gmail.com>
2016-10-19 23:39:18 +0200

    a

--- server/config/settings.py
+++ server/config/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import dj_database_url
-import custom_storages
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
