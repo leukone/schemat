c06fe0ae0a158b7284278ff6827118d4657c2974
leukone <scisly.a@gmail.com>
2016-10-24 04:48:27 +0200

    a

--- server/website/admin.py
+++ server/website/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Project, Info, ContactInfo
+from .models import Project, Info, ContactInfo, MiniImage
 
 admin.site.register(Project)
 admin.site.register(ContactInfo)
 admin.site.register(Info)
+admin.site.register(MiniImage)
