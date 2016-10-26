7ac1786261f2d561d35c7e2b94102f93d8dd7e2e
leukone <scisly.a@gmail.com>
2016-10-26 17:11:09 +0200

    a

--- server/website/views.py
+++ server/website/views.py
@@ -12,7 +12,7 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
-class ProjectList(GenericAPIView):
+class ProjectList(APIView):
 
 	def get(self, request):
 		projects = Project.objects.all()
