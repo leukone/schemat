794fb761d743f834c6be537275c059a3b068170c
leukone <scisly.a@gmail.com>
2016-10-26 22:13:32 +0200

    a

--- server/website/views.py
+++ server/website/views.py
@@ -17,7 +17,7 @@ class ProjectList(generics.ListCreateAPIView):
 	serializer_class = ProjectSerializer
 	def post(self, request, format=None):
 		serializer = ProjectSerializer(data=request.DATA, files=request.FILES)
-    	if serializer.is_valid():
+		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
