eecaae006d99cd9e66ba9cd0d75d51309f2c0753
leukone <scisly.a@gmail.com>
2016-10-27 18:03:02 +0200

    a

--- client/angular2.0webpack/src/app/project.ts
+++ client/angular2.0webpack/src/app/project.ts
@@ -2,14 +2,25 @@ export class Project {
     ident: number;
     title: string;
     desc: string;
-    projectName: string;
     image: string;
+    gallery1:string;
+    gallery2:string;
+    gallery3:string;
+    gallery4:string;
+    gallery5:string;
+    gallery6:string;
 
-    constructor (ident: number, title: string, desc: string, projectName: string, image: string) {
+
+    constructor (ident: number, title: string, desc: string, image: string, gallery1:string, gallery2:string, gallery3:string, gallery4:string, gallery5:string, gallery6:string) {
         this.ident = ident;
         this.title = title;
         this.desc = desc;
-        this.projectName = projectName;
         this.image = image;
+        this.gallery1 = gallery1;
+        this.gallery2 = gallery2;
+        this.gallery3 = gallery3;
+        this.gallery4 = gallery4;
+        this.gallery5 = gallery5;
+        this.gallery6 = gallery6;
     }
 }
