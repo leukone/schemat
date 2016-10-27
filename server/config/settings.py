dbb8ae6e47a67540faa5ac701569b074dccfa681
leukone <scisly.a@gmail.com>
2016-10-27 14:47:17 +0200

    a

--- server/config/settings.py
+++ server/config/settings.py
@@ -23,8 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 
 # Application definition
 
